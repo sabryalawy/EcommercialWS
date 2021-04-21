98b8141a47dc844cdb208f05d8ce9021c2d77b11
Sabry alawy <sabry0595911611@gmail.com>
2021-04-21 21:26:30 +0300

    initalizing

---
+++ src/main/java/com/example/Ecommercial/EcommercialApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ecommercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ecommercial/EcommercialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ecommercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

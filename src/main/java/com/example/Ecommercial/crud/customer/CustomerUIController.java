cf8cd382e960a3bea16b99b2c216c35823f8b86f
Sabry alawy <sabry0595911611@gmail.com>
2021-04-25 07:00:53 +0300

    jsp is now working

--- src/main/java/com/example/Ecommercial/crud/customer/CustomerUIController.java
+++ src/main/java/com/example/Ecommercial/crud/customer/CustomerUIController.java
@@ -1,14 +1,15 @@
 package com.example.Ecommercial.crud.customer;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CustomerUIController {
 
 
-    @GetMapping("/customerui")
+    @RequestMapping("/customerui")
     public String customerUI(){
-        return "UI/customerUI/customerui";
+        return "home";
     }
 }
